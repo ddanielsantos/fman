73a1d905915da53ec14c9fa3c1bbcd750dff39ad
ddanielsantos <80872981+ddanielsantos@users.noreply.github.com>
2024-10-25 08:49:08 -0300

    refactor: improve log file path

--- src/debug.rs
+++ src/debug.rs
@@ -1,23 +1,59 @@
-use std::fs::File;
+use std::{
+    fs::{self, File},
+    path::PathBuf,
+};
 
 use color_eyre::{eyre::Context, Result};
+use directories::ProjectDirs;
+use lazy_static::lazy_static;
 use tracing::Level;
 use tracing_appender::{non_blocking, non_blocking::WorkerGuard};
 use tracing_subscriber::EnvFilter;
 
+lazy_static! {
+    pub static ref PROJECT_NAME: String = env!("CARGO_CRATE_NAME").to_uppercase().to_string();
+    pub static ref DATA_FOLDER: Option<PathBuf> =
+        std::env::var(format!("{}_DATA", PROJECT_NAME.clone()))
+            .ok()
+            .map(PathBuf::from);
+    pub static ref LOG_ENV: String = format!("{}_LOGLEVEL", PROJECT_NAME.clone());
+    pub static ref LOG_FILE: String = format!("{}.log", env!("CARGO_PKG_NAME"));
+}
+
 pub fn initialize_logging() -> Result<WorkerGuard> {
-    let file = File::create("tracing.log").wrap_err("failed to create tracing.log")?;
+    let (dir, file_path) = get_log_file_path();
+    std::fs::create_dir_all(dir)?;
+
+    let file = File::create(file_path).wrap_err("failed to create log file")?;
     let (non_blocking, guard) = non_blocking(file);
 
-    // By default, the subscriber is configured to log all events with a level of `DEBUG` or higher,
-    // but this can be changed by setting the `RUST_LOG` environment variable.
     let env_filter = EnvFilter::builder()
         .with_default_directive(Level::DEBUG.into())
         .from_env_lossy();
 
     tracing_subscriber::fmt()
+        .with_file(true)
+        .with_line_number(true)
         .with_writer(non_blocking)
         .with_env_filter(env_filter)
         .init();
+
     Ok(guard)
+}
+
+fn get_log_file_path() -> (PathBuf, PathBuf) {
+    let dir = {
+        if let Some(pt) = DATA_FOLDER.clone() {
+            pt
+        } else if let Some(dir) = ProjectDirs::from("com", "ddanielsantos", env!("CARGO_PKG_NAME"))
+        {
+            dir.data_local_dir().to_path_buf()
+        } else {
+            PathBuf::from(".").join(".data")
+        }
+    };
+
+    let file_path = dir.join(LOG_FILE.clone());
+
+    (dir, file_path)
 }
